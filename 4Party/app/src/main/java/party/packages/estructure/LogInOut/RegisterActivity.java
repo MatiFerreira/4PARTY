adf2583811bc65ec7e00693d9d4b17409c9d4e4e
Mathias Ferreira Ferrreira <maziuseven@gmail.com>
2023-04-16 19:29:33 +0200

    INTACIA DE APP ANDROID STUDIO

---
+++ 4Party/app/src/main/java/party/packages/estructure/LogInOut/RegisterActivity.java
@@ -0,0 +1,16 @@
+package party.packages.estructure.LogInOut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import party.packages.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
