e9baf601e0ec16a1144321611226ae19d24d2e0d
Mathias Ferreira Ferrreira <maziuseven@gmail.com>
2023-04-17 18:27:52 +0200

    SDK NUEVOS

---
+++ 4party/app/src/main/java/com/example/a4party/HOME_ACTIVITY.java
@@ -0,0 +1,13 @@
+package com.example.a4party;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HOME_ACTIVITY extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
