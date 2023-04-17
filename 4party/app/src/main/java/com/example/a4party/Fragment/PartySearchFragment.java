8ca6f17d51523f0ce94fa9b0f8f711e382312113
Mathias Ferreira Ferrreira <maziuseven@gmail.com>
2023-04-17 20:52:12 +0200

    IMPLEMENTACION DE FRAGMENT CON SU RESPECTIVO ACTION BAR

---
+++ 4party/app/src/main/java/com/example/a4party/Fragment/PartySearchFragment.java
@@ -0,0 +1,19 @@
+package com.example.a4party.Fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.a4party.R;
+
+
+public class PartySearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_party_search, container, false);
+    }
+}
