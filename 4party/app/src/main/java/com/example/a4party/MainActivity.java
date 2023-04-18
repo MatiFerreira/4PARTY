95278a94c1efdd26dbc9cd408a9585b62e93531b
Mathias Ferreira Ferrreira <maziuseven@gmail.com>
2023-04-18 14:14:03 +0200

    SE HA AÑADIDO PLANTILLA SPLASH SCREEN IMAGENS Y NUEVO MAIN

---
+++ 4party/app/src/main/java/com/example/a4party/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a4party;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HOME_ACTIVITY.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 8000);
+    }
+}
