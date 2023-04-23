f2fdcbbb245b8a167876aee9c7ccff014e3a294d
Nacho <igp0003@alu.medac.es>
2023-04-23 20:22:30 +0200

    hecho crud y usuario

---
+++ Usuario.java
@@ -0,0 +1,26 @@
+package com.example.a4party;
+
+public class Usuario {
+
+    private String nombre;
+    private String apellido;
+    private String DNI;
+    private String email;
+    private String contrasenia;
+
+
+    public Usuario(String nombre, String apellido, String DNI, String email, String contrasenia) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.DNI = DNI;
+        this.email = email;
+        this.contrasenia = contrasenia;
+    }
+
+    public Usuario (){
+
+    }
+
+
+
+}
