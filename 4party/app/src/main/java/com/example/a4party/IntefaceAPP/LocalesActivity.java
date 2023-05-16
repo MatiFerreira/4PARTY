2fa79f8818b61830867ab3277f81c2c39b4454d2
Mathias Ferreira Ferrreira <maziuseven@gmail.com>
2023-05-16 10:39:05 +0200

    PUEDE INSERTAR IMAGEN Y ADEMAS CARGA LOS DATOS

--- 4party/app/src/main/java/com/example/a4party/IntefaceAPP/LocalesActivity.java
+++ 4party/app/src/main/java/com/example/a4party/IntefaceAPP/LocalesActivity.java
@@ -2,25 +2,37 @@ package com.example.a4party.IntefaceAPP;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.a4party.BBDD.CRUD;
 import com.example.a4party.LogInOutBusiness.LogInBusiness;
 import com.example.a4party.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
 
 public class LocalesActivity extends AppCompatActivity {
     private TextView textViewTitulo;
     private FirebaseAuth firebaseAuth;
     private Button botonOferta, botonModificarDatos, botonImagen, botonCerrarSesion, botonEliminarcuenta;
     private CRUD crud;
+    private Uri urimage;
+    private static final int IMG = 1;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_home_empresario);
@@ -36,6 +48,7 @@ public class LocalesActivity extends AppCompatActivity {
         botonEliminarcuenta = findViewById(R.id.botonteliminarcuenta);
         LogOut();
         EliminarCuenta(emailuser, user);
+        addImg();
     }
 
 
@@ -76,7 +89,33 @@ public class LocalesActivity extends AppCompatActivity {
         });
     }
 
-    private void getImage() {
+    private void addImg() {
+        botonImagen.setOnClickListener(view -> {
+            intentImg();
+        });
+    }
 
+    private void intentImg() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(intent, IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMG && resultCode == RESULT_OK) {
+            urimage = data.getData();
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("img").child("Empresarios" + urimage.getLastPathSegment());
+            storageRef.putFile(urimage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //Aqui lo meto en firestore
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    String emailuser = user.getEmail();
+                    crud.updateImage(urimage, LocalesActivity.this, emailuser);
+                }
+            });
+        }
     }
 }
