cb5241c77f7f2bf36065aa0735f14a266bd1926e
iker <irg0004@alu.medac.es>
2023-05-29 11:53:25 +0200

    Carrito

--- 4party/app/src/main/java/com/example/a4party/Fragment/OrderFragments.java
+++ 4party/app/src/main/java/com/example/a4party/Fragment/OrderFragments.java
@@ -1,19 +1,53 @@
 package com.example.a4party.Fragment;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.example.a4party.R;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.a4party.R;
+import com.example.a4party.databinding.FragmentOrderBinding;
+import com.example.a4party.viewModels.ShopViewModel;
 
 public class OrderFragments extends Fragment {
+
+    NavController navController;
+    FragmentOrderBinding fragmentOrderBinding;
+    ShopViewModel shopViewModel;
+
+    public OrderFragments() {
+        // Required empty public constructor
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_fragments, container, false);
+//        return inflater.inflate(R.layout.fragment_order, container, false);
+        fragmentOrderBinding = FragmentOrderBinding.inflate(inflater, container, false);
+        return fragmentOrderBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        shopViewModel = new ViewModelProvider(requireActivity()).get(ShopViewModel.class);
+
+        fragmentOrderBinding.continueShoppingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shopViewModel.resetCart();
+                navController.navigate(R.id.action_orderFragment_to_shopFragment);
+            }
+        });
     }
 }
