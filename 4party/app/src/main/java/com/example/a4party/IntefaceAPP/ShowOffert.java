0165fb42391d66c2c02b0214eafa9233714f98f4
Miguel Angel <miguelfm03@gmail.com>
2023-05-29 21:01:29 +0200

    Revert "MOSTRAR OFERTA Y CAMBIOS"
    This reverts commit 4fb4f25ccbf1c9e5a3613d7ccfe0616351dc3f24.

--- 4party/app/src/main/java/com/example/a4party/IntefaceAPP/ShowOffert.java
+++ 4party/app/src/main/java/com/example/a4party/IntefaceAPP/ShowOffert.java
@@ -7,12 +7,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.a4party.R;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ShowOffert extends AppCompatActivity {
 
-    private TextView descripcioEstable, precioEstable, nameLocal;
-    private FirebaseFirestore db;
+    private TextView descripcioEstable, precioEstable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,28 +18,5 @@ public class ShowOffert extends AppCompatActivity {
         setContentView(R.layout.activity_mostrar_ofertas);
         descripcioEstable = findViewById(R.id.textView11);
         precioEstable = findViewById(R.id.preciotxtv);
-        nameLocal = findViewById(R.id.namLocal);
-        Bundle datosEmail = getIntent().getExtras();
-        String email = datosEmail.getString("email");
-        db = FirebaseFirestore.getInstance();
-        db.collection("Productos").document(email).get().addOnSuccessListener(documentSnapshot -> {
-            if (documentSnapshot.exists()) {
-                String precio = documentSnapshot.getString("precio");
-                String decription = documentSnapshot.getString("descripcion");
-                descripcioEstable.setText(decription);
-                precioEstable.setText(precio);
-            } else {
-                descripcioEstable.setText("");
-                precioEstable.setText("0.0");
-            }
-        });
-        db.collection("Empresarios").document(email).get().addOnSuccessListener(documentSnapshot -> {
-            if (documentSnapshot.exists()) {
-                String nameEstablecimiento = documentSnapshot.getString("nombreEstablecimiento");
-                nameLocal.setText(nameEstablecimiento);
-            } else {
-                nameLocal.setText("");
-            }
-        });
     }
 }
